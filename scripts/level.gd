90aad56e250eb6f17d368a2dfd6fb0e393d87bf8
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-20 20:49:32 +1000

    stuff

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-# get child node so we can rotate it later
-@onready var Logo = $Logo
-
 var test_array: Array[String] = ["Carly", "Lizey", "Immy"]
  
-func _ready():
-	Logo.rotation_degrees = 45	
-	
+func _ready():		
 	# print whats in the array
-	print(test_array[1])
-		
+	print(test_array[1])	
 	
 
 func _process(_delta):
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends CharacterBody2D
 
-const PLAYER_SPEED = 350
+const PLAYER_SPEED = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,10 +8,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	# get input
-	var direction = Input.get_vector("left", "right", "forward", "backward")
-	position += direction * PLAYER_SPEED * delta
+	var direction = Input.get_vector("left", "right", "forward", "backward")	
+	velocity = direction * PLAYER_SPEED
+	# delta is automatically included with move and slide
+	move_and_slide()	
 
 	# fire
 	if Input.is_action_pressed("primary action"):
